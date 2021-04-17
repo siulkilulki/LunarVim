986283fb789a48165438360c0885abde63b2c2a2
Dawid Jurkiewicz <dawid.jurkiewicz@applica.ai>
2021-04-17 02:43:32 +0200

    remap : to ;

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -1,5 +1,6 @@
 vim.api.nvim_set_keymap('n', '<Space>', '<NOP>', {noremap = true, silent = true})
 vim.g.mapleader = ' '
+vim.api.nvim_set_keymap('', ';', ':', {noremap = true})
 
 -- no hl
 vim.api.nvim_set_keymap('n', '<Leader>h', ':set hlsearch!<CR>', {noremap = true, silent = true})
