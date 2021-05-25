b07fe4472d4a2fa6f3e66c3708a16e19e2e122fa
Dawid Jurkiewicz <dawid.jurkiewicz@applica.ai>
2021-05-25 17:51:02 +0200

    Modify keymappings for barbar and windows

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -11,6 +11,7 @@ vim.api.nvim_set_keymap('n', '<Leader>e', ':NvimTreeToggle<CR>', {noremap = true
 vim.api.nvim_set_keymap('n', '-', ':RnvimrToggle<CR>', {noremap = true, silent = true})
 
 -- better window movement
+vim.api.nvim_set_keymap('n', '<TAB>', '<C-w>w', {silent = true})
 vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', {silent = true})
 vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', {silent = true})
 vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', {silent = true})
@@ -32,12 +33,12 @@ vim.cmd([[
 
 -- TODO fix this
 -- resize with arrows
-vim.cmd([[
-  nnoremap <silent> <C-Up>    :resize -2<CR>
-  nnoremap <silent> <C-Down>  :resize +2<CR>
-  nnoremap <silent> <C-Left>  :vertical resize -2<CR>
-  nnoremap <silent> <C-Right> :vertical resize +2<CR>
-]])
+-- vim.cmd([[
+--   nnoremap <silent> <C-Up>    :resize -2<CR>
+--   nnoremap <silent> <C-Down>  :resize +2<CR>
+--   nnoremap <silent> <C-Left>  :vertical resize -2<CR>
+--   nnoremap <silent> <C-Right> :vertical resize +2<CR>
+-- ]])
 
 -- better indenting
 vim.api.nvim_set_keymap('v', '<', '<gv', {noremap = true, silent = true})
@@ -49,8 +50,8 @@ vim.api.nvim_set_keymap('i', 'kj', '<ESC>', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('i', 'jj', '<ESC>', {noremap = true, silent = true})
 
 -- Tab switch buffer
-vim.api.nvim_set_keymap('n', '<TAB>', ':bnext<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<S-TAB>', ':bprevious<CR>', {noremap = true, silent = true})
+-- vim.api.nvim_set_keymap('n', '<C-Left>', ':bnext<CR>', {noremap = true, silent = true})
+-- vim.api.nvim_set_keymap('n', '<C-Right>', ':bprevious<CR>', {noremap = true, silent = true})
 
 -- Move selected line / block of text in visual mode
 vim.api.nvim_set_keymap('x', 'K', ':move \'<-2<CR>gv-gv', {noremap = true, silent = true})
